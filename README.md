1c31e511c467ddd2052cb3483acbf9de81fdbec1
HM <hafeez.itm@gmail.com>
2023-12-31 12:48:13 -0500

    re-arranged folder structure

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+
+This is the simplest of all terraform code primarily to test and demonstrate the setup.
+Here we are creating:
+
+-App Service Plan (ASP)
+-App Service
+-Logic Apps
---
+++ modules/app-service/main.tf
@@ -0,0 +1,66 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+# Configure the Azure provider
+provider "azurerm" {
+  environment = "public"
+  version     = ">= 2.15.0"
+  features {}
+}
+
+# Create a Resource Group
+resource "azurerm_resource_group" "appservice-rg" {
+  name = "${var.app_name}-${var.environment}_AppService-rg"
+  location = var.location
+
+  tags = {
+    owner = var.owner
+    environment = var.environment
+  }
+}
+
+# Create app service plan
+resource "azurerm_app_service_plan" "service-plan" {
+  name = "${var.app_name}-${var.environment}_AppService-plan"
+  location = azurerm_resource_group.appservice-rg.location
+  resource_group_name = azurerm_resource_group.appservice-rg.name
+  kind = "Linux"
+  reserved = true
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+
+  tags = {
+    owner = var.owner
+    environment = var.environment
+  }
+}
+
+# Create app service
+resource "azurerm_app_service" "app-service" {
+  name = "${var.app_name}-${var.environment}"
+  location = azurerm_resource_group.appservice-rg.location
+  resource_group_name = azurerm_resource_group.appservice-rg.name
+  app_service_plan_id = azurerm_app_service_plan.service-plan.id
+
+  site_config {
+    linux_fx_version = "DOCKER|${var.docker_image}:${var.docker_image_tag}"
+  }
+
+  app_settings = {
+    "WEBSITES_PORT" = var.app_port
+  }
+
+  tags = {
+    owner = var.owner
+    environment = var.environment
+  }
+}
+
+output "app_service_url" {
+  value       = azurerm_app_service.app-service.default_site_hostname
+  description = "Default URL to access the app service."
+}
